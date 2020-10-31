fe9742d13283526efb7bc67f606f9f13f45bbbc8
HendrikCromboom <hcromboom@gmail.com>
2020-10-31 12:10:52 +0100

    added levelup for player

--- Model/Player.cs
+++ Model/Player.cs
@@ -9,11 +9,16 @@ namespace Sharp.Model
         private string name;
         private int maxHealth;
         private int health;
-
+        private int level;
+        private int exp;
+        private int maxExp;
         public Player()
         {
             this.maxHealth = 30;
             this.health = 30;
+            this.level = 1;
+            this.exp = 0;
+            this.maxExp = 10;
         }
 
         public void setName(string name)
@@ -31,6 +36,15 @@ namespace Sharp.Model
         public bool isAlive()
         {
             return this.health < 0 ;
+        }
+        public void checkLevelUp()
+        {
+            if(this.exp >= this.maxExp)
+            {
+                this.exp = this.exp - this.maxExp;
+                this.maxExp = this.maxExp + (5 * this.level);
+                this.level++;
+            }
         }
     }
 }
