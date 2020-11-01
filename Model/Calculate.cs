33c8145777df954f6d7152e217aad6f8f1fdfe08
HendrikCromboom <hcromboom@gmail.com>
2020-11-01 13:10:32 +0100

    randomizer added, hitchance, calculating

---
+++ Model/Calculate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharp.Model
+{
+    class Calculate
+    {
+        private Random random = new Random(101);
+        private int randomize()
+        {
+            
+            return this.random.Next();
+        }
+        public bool hitChance()
+        {
+            if(this.randomize() < 50)
+            {
+                return false;
+            }
+            else { return true; }
+        }
+    }
+}
